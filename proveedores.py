59147c5c2b94611afccc56d8dd7cb9703fc01b15
Moraless0 <moraless06@hotmail.com>
2026-05-08 03:33:41 -0600

    feat: :sparkles: Se agrega la funcion registrar proveedor

--- proveedores.py
+++ proveedores.py
@@ -11,3 +11,35 @@ def guardar(data):
 
 def _buscar_por_codigo(data, codigo):
     return next((p for p in data if p["codigo"] == codigo), None)
+
+def registrar():
+    data = cargar()
+    print("\n=== Registrar Proveedor ===")
+
+    while True:
+        codigo = non_empty("Código: ")
+        if unique_code(codigo, data, "codigo"):
+            break
+        print("⚠ Código repetido.")
+
+    empresa = non_empty("Empresa: ")
+    contacto = non_empty("Contacto: ")
+    direccion = non_empty("Dirección: ")
+    tel_cel = phone("Teléfono celular: ")
+    tel_fijo = phone("Teléfono fijo: ")
+    correo = email("Correo: ")
+
+    item = {
+        "codigo": codigo,
+        "empresa": empresa,
+        "contacto": contacto,
+        "direccion": direccion,
+        "tel_cel": tel_cel,
+        "tel_fijo": tel_fijo,
+        "correo": correo,
+        "historial_transacciones": []
+    }
+
+    data.append(item)
+    guardar(data)
+    print("✅ Registrado.")
