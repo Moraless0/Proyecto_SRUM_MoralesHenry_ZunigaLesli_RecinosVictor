eb468d6317ee6820da461f10a6fb6851b9a33e62
Moraless0 <moraless06@hotmail.com>
2026-05-08 03:31:30 -0600

    feat: :sparkles: Se agregan las funciones de carga de archivos

---
+++ proveedores.py
@@ -0,0 +1,10 @@
+from persistence import load_json, save_json
+from validators import non_empty, email, phone, unique_code
+
+FILE = "data/proveedores.json"   # ← RUTA ACTUALIZADA
+
+def cargar():
+    return load_json(FILE, [])
+
+def guardar(data):
+    save_json(FILE, data)
